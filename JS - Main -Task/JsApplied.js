ca722cdf9f601fee59db7f5eb52fe264b3b41cb0
Bhargavi Dogra <bhargavi_dogra@epam.com>
2020-03-12 13:05:37 +0530

    youtube task commit

--- JS - Main -Task/JsApplied.js
+++ JS - Main -Task/JsApplied.js
@@ -1,4 +1,4 @@
-var maxRes = 15; 
+var maxRes = 15;  var counter = 0; var data;
  function searchQ(){
       query = document.getElementById('queryText').value;
       email = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=15'+
@@ -15,47 +15,66 @@ var maxRes = 15;
       document.getElementsByTagName('head')[0].appendChild(s);
   }
     
+
+
   function myPlan(response){
     console.log(response);
+   
+    data = response;
+ console.log(counter);  
 
-
-          for (var i=0; i<3;i++){
-      
-           var videoID=response.items[i].id.videoId;
-          //  if(typeof videoID != 'undefined'){    
-           var title=response.items[i].snippet.title;
-      //      var links = '<br><img src="http://img.youtube.com/vi/'+ videoID + 
-      //           '/default.jpg" width="80" height="60">'+
-      //           ''+(i+1)+ '.&nbsp;<a href="#" onclick="playVid(\''+ videoID + 
-      //            '\');return false;">'+ title + '</a><br>';
-
-      //     document.getElementById('list1a').innerHTML += links ;
-      //     }
-      // }
-
-           let mainDiv =document.querySelector('#list1a');
-          let div = document.createElement('div');
-          div.classList.add('container');
-          let img = document.createElement('img');
-          let imsource=response.items[i].snippet.thumbnails.medium.url;
-             img.setAttribute('src',imsource);
-           div.appendChild(img);
-          let text1 = document.createTextNode(title);
-      //     div.appendChild(text1);
-          let a = document.createElement('a');
-          var link = document.createTextNode(title); 
-          a.href = "https://www.youtube.com/watch?v="+videoID;  
-          a.appendChild(link);
-          div.appendChild(a);
-          mainDiv.appendChild(div);
-          }
+    print(data);
     
     }
 
 
-    
+
+function print(response) {
+      let mainDiv = document.querySelector('#list1a');
+      mainDiv.innerHTML = "";
+      for (var i = (counter * 3); i < ((counter * 3) + 3) && (i < maxRes); i++) {
+            var videoID = response.items[i].id.videoId;
+            var title = response.items[i].snippet.title;
+            let div = document.createElement('div');
+            div.classList.add('container');
+            let img = document.createElement('img');
+            let imsource = response.items[i].snippet.thumbnails.medium.url;
+            img.setAttribute('src', imsource);
+            div.appendChild(img);
+            let a = document.createElement('a');
+            var link = document.createTextNode(title);
+            a.href = "https://www.youtube.com/watch?v=" + videoID;
+            a.appendChild(link);
+            div.appendChild(a);
+            mainDiv.appendChild(div);
+      }
+}
 
 
+      let forwardCount = document.getElementById('nextButton');
+      forwardCount.addEventListener('click', event => {
+            counter++;
+            console.log(counter);
+            if(((counter * 3) + 3)>15)
+            {
+                  counter = 4;
+                  return;
+            }else{
+             print(data);
+            }
+      });
+
+      let backwordCount = document.getElementById('previousButton');
+      backwordCount.addEventListener('click', event => {
+            counter--;
+            console.log(counter);
+
+            if(counter <0){
+                  counter = 0;
+                  return ;
+            }else{
+             print(data);
+            }
+      });
 
 
- 
